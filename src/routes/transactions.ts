57b849a712eeaf6fbb5869251291c919c77f8985
Dennis Costa <dennis.costa@banpar.com.br>
2024-09-09 16:28:06 -0300

    rota para criar transacoes com validacao zod

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,16 +3,33 @@
 
 //import para definir o tipo de app (parametro do plugin)
 import { FastifyInstance } from "fastify"
-
+import { z } from "zod"
 //importar de database e nao de knex diretamente
 import { knex } from "../database"
+import { randomUUID } from "node:crypto"
 
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.get('/hello', async () => {
-        const transactions = await knex('transactions')
-            .where('amount', 1000)
-            .select('*')
+    app.post('/', async (request, response) => {
+        //{ title, amount, type: credit or debit}
+        
+        //vamos utilizar o zod para definir e validar o campos enviados pelo request !!IMPORTANTE!!
+        //essa validacao é importante para que ao salvar no banco de dados, os dados sejam integros
+        const requestTransactionBody = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit'])
+        })
+
+        // caso tenha infos invalidas, ele vai jogar um erro
+        const { title, amount, type } = requestTransactionBody.parse(request.body)
     
-        return transactions
+        await knex('transactions')
+            .insert({
+                id: randomUUID(),
+                title,
+                amount: type === 'credit' ? amount : (amount * -1)
+            })
+
+        return response.status(201).send()
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,12 @@ import { transactionsRoutes } from "./routes/transactions";
 const app = fastify();
 
 //utilizamos o app.register para ler o plugin que contem nossas rotas
-app.register(transactionsRoutes)
+//prefix define qual o endpoint padrao dessas rotas, nesse caso transaction
+//dentro do plugin, passa nao ser necessario informar transaction
+//apenas do transaction em diante /, /:id etc
+app.register(transactionsRoutes, {
+    prefix: 'transactions'
+})
 
 app.listen({
     port: env.PORT,
